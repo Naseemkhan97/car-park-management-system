48b93e62517801fa191de6b4869ad83ca28dc2d7
Naseemkhan97 <56490966+Naseemkhan97@users.noreply.github.com>
2020-12-10 20:15:43 +0000

    Update CarParkState.java

--- src/CarParkState.java
+++ src/CarParkState.java
@@ -18,16 +18,16 @@ public class CarParkState{
 		//Attempt to acquire a lock
 		Thread me = Thread.currentThread(); // get a ref to the current thread
 		System.out.println("");
-	    System.out.println(me.getName()+" is attempting to acquire a lock!");	
-	    ++threadsWaiting;
+		System.out.println(me.getName()+" is attempting to acquire a lock!");	
+	    	++threadsWaiting;
 		
-	    while (accessing) {  // while someone else is accessing or threadsWaiting > 0
-	    	System.out.println(me.getName()+" waiting to get a lock as someone else is accessing...");
-		    //wait for the lock to be released - see releaseLock() below
-		    wait();
-	    }
+	    	while (accessing) {  // while someone else is accessing or threadsWaiting > 0
+	    		System.out.println(me.getName()+" waiting to get a lock as someone else is accessing...");
+		    	//wait for the lock to be released - see releaseLock() below
+		    	wait();
+	    	}
 		// nobody has got a lock so get one
-	    --threadsWaiting;
+	    	--threadsWaiting;
 		accessing = true;
 		System.out.println(me.getName()+" got a lock!"); 
 	}
@@ -37,69 +37,67 @@ public class CarParkState{
 		accessing = false;
 		notifyAll();
 		Thread me = Thread.currentThread(); // get a ref to the current thread
-		System.out.println(me.getName()+" released a lock!");
-		
+		System.out.println(me.getName()+" released a lock!");	
 	}
 	
 	public synchronized String processInput(String myThreadName, String theInput) {
 		
 		System.out.println(myThreadName + " received "+ theInput);
-    	String theOutput = null;
-    	// verify what the client said
-    	if (theInput.equalsIgnoreCase("Enter car park")||theInput.equalsIgnoreCase("Exit car park")) {
-    		// if the client enters correct request, check which client sent the request
-    		if (myThreadName.equals("CPServerThread1")) {
-    			//only add a car to car park if it is less then 5,	
-    			if (carParkCapacity<5) {
-    				carParkCapacity = carParkCapacity + 1;
-    			}
-    			else{
-    			//otherwise, make the car wait	
-    				carsWaiting = carsWaiting + 1;
-    			}
-    			//let the client know how many cars are in the car park and waiting
-    			System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
-    			theOutput = "a car has entered. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
+    		String theOutput = null;
+    		// verify what the client said
+    		if (theInput.equalsIgnoreCase("Enter car park")||theInput.equalsIgnoreCase("Exit car park")) {
+    			// if the client enters correct request, check which client sent the request
+    			if (myThreadName.equals("CPServerThread1")) {
+    				//only add a car to car park if it is less then 5,	
+    				if (carParkCapacity<5) {
+    					carParkCapacity = carParkCapacity + 1;
+    				}
+    				else{
+    					//otherwise, make the car wait	
+    					carsWaiting = carsWaiting + 1;
+    				}
+    				//let the client know how many cars are in the car park and waiting
+    				System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
+    				theOutput = "a car has entered. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
     				
-    		}
-    		else if (myThreadName.equals("CPServerThread2")) {
+    			}
+    			else if (myThreadName.equals("CPServerThread2")) {
     				
-    			if (carParkCapacity<5) {
-    				carParkCapacity = carParkCapacity + 1;
+    				if (carParkCapacity<5) {
+    					carParkCapacity = carParkCapacity + 1;
+    				}	
+    				else{
+    					carsWaiting = carsWaiting + 1;
+    				}
+    				System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
+    				theOutput = "a car has entered. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
     			}
-    			else{
-    				carsWaiting = carsWaiting + 1;
-    			}
-    			System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
-    			theOutput = "a car has entered. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
-
-    		}
-       		else if (myThreadName.equals("CPServerThread3")) {
-       			//for the exit clients, minus a car from car park	
-       			carParkCapacity = carParkCapacity - 1;
+       			else if (myThreadName.equals("CPServerThread3")) {
+       				//for the exit clients, minus a car from car park	
+       				carParkCapacity = carParkCapacity - 1;
 					
 				if (carsWaiting>0) {
 					//minus a car from waiting and add a car to car park
-    				carsWaiting = carsWaiting - 1;
-    				carParkCapacity = carParkCapacity + 1;
+    					carsWaiting = carsWaiting - 1;
+    					carParkCapacity = carParkCapacity + 1;
 				}
-    			//let the client know how many cars are in the car park and waiting
-       			System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
-    			theOutput = "a car has exited. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
+    				//let the client know how many cars are in the car park and waiting
+       				System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
+    				theOutput = "a car has exited. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
 
-       		}
-       		else if (myThreadName.equals("CPServerThread4")) {
+       			}
+       			else if (myThreadName.equals("CPServerThread4")) {
     			
-       			carParkCapacity = carParkCapacity - 1;
+       				carParkCapacity = carParkCapacity - 1;
 					
 				if (carsWaiting>0) {
-    				carsWaiting = carsWaiting - 1;
-    				carParkCapacity = carParkCapacity + 1;
+    					carsWaiting = carsWaiting - 1;
+    					carParkCapacity = carParkCapacity + 1;
 				}
-    			System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
-    			theOutput = "a car has exited. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
+    				System.out.println(myThreadName + " There are " + carParkCapacity + " cars in the car park.");
+    				theOutput = "a car has exited. Car park capacity now: " + carParkCapacity + " | Cars Waiting: " + carsWaiting;
 
-       		}
+       			}
        		else {
        			System.out.println("DO NOT RECOGNISE THREAD CALL!");
        		}
